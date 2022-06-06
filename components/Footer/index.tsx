bbae795c224f1ad2147d84284e8044fc3b3ad457
moaitodev <bymavc@outlook.com>
2022-06-05 23:06:16 -0500

    🐛 Removed unused imports

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,20 +1,11 @@
-import { footerLinks } from "../../config/menus"
 import { brandLabel, devLabel } from "../../config/labels"
-import FooterItem from "./FooterItem"
 import styles from "./styles.module.css"
-
-const navigation = footerLinks
 
 const Footer = (): JSX.Element => {
   return (
     <>
       <div className={styles.MainFooter}>
         <div className={styles.MainFooter__Container}>
-          {/* <div className={styles.MainFooterLinks}>
-            {navigation.links.map(({ id, to, label }) => (
-              <FooterItem key={id} to={to} label={label} />
-            ))}
-          </div> */}
           <div className={styles.MainFooterLabel}>
             <div className={styles.PageLabel}>{brandLabel}</div>
             <div className={styles.DevBrand}>{devLabel}</div>
